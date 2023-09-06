d1231f9104ba0f361f7a27a5716d6f09589c51d6
Vaibhs29 <yadavvaibhav9890@gmail.com>
2023-09-06 14:20:24 +0530

    done

---
+++ Main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_vpc" "MyVpc" {
+    cidr_block = "15.15.0.0/16"
+   
+    tags = {
+        Name = "MyVpc"
+      
+    }
+  }
