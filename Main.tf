b2ea89c3d6dc2d31f20900ea6a889c35ec873402
Vaibhs29 <yadavvaibhav9890@gmail.com>
2023-09-07 11:01:55 +0530

    subnet has been added

--- Main.tf
+++ Main.tf
@@ -9,11 +9,21 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_vpc" "MyVpc" {
+resource "aws_vpc" "MyVpc_New" {
     cidr_block = "15.15.0.0/16"
    
     tags = {
-        Name = "MyVpc_byJenkins"
+        Name = "New_MyVpc_byJenkins"
+      
+    }
+  }
+  resource "aws_subnet" "Subnet-1" {
+    vpc_id = aws_vpc.MyVpc_New.id
+    cidr_block = "15.15.1.0/24"
+    availability_zone = "ap-south-1a"
+
+    tags = {
+        Name = "Subnet-1"
       
     }
   }
